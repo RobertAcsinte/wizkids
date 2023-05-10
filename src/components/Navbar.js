0b3a1fc819d5933644abc2bbf1abcb203139a4e2
Robert Acsinte <robert.acsinte@gmail.com>
2023-05-10 13:41:50 +0200

    Cleaned unused files | navbar (design wip)

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav>
+      <a className='company-logo-navbar' href="/">OWOW</a>
+      <a className='login-text-navbar' href="/">Login</a>
+    </nav>
+  );
+}
+
+export default Navbar;
