89154f358fc4e7a2349c5211c34bee2d04f4fb40
Robert Acsinte <robert.acsinte@gmail.com>
2023-05-12 14:21:08 +0200

    Loading spinner for list of wizkids | Shows error message if fetching failed

--- src/components/WizkidsList.js
+++ src/components/WizkidsList.js
@@ -3,24 +3,43 @@ import WizkidsElement from './WizkidsElement';
 import { useContext } from 'react';
 import WizkidsContext from "../context/wizkids";
 import { Link } from 'react-router-dom';
+import { SyncLoader } from 'react-spinners';
 
 function WizkidsList() {
 
-  const {wizkids} = useContext(WizkidsContext);
+  const {wizkids, loading, error} = useContext(WizkidsContext);
 
   const renderedWizkids = wizkids.map((wizkid) => {
     return (
       <div className='center-wizkids-grid' key={wizkid.id}>
-        
+        <div>
+        </div>
         <WizkidsElement wizkid={wizkid}/>
       </div>
     )
   });
 
+
+  if(loading) {
+    return (
+      <div className='wizkids-list-container-error-loading'>
+        <SyncLoader color="var(--owow-green)" size="80px" />
+      </div>
+    );
+  }
+
+  if(error != "") {
+    return (
+      <div className='wizkids-list-container-error-loading'>
+        {error}
+      </div>
+    );
+  }
+
   return (
     <div className='wizkids-list-container'>
       {renderedWizkids}
-    </div>
+    </div>  
   );
 }
 
--- src/context/wizkids.js
+++ src/context/wizkids.js
@@ -6,10 +6,25 @@ const WizkidsContext = createContext();
 
 function Provider({children}) {
   const [wizkids, setWizkids] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const fetchWizkids = async () => {
-    const response = await axios.get("http://localhost:3001/wizkids")
-    setWizkids(response.data)
+  //delay to simulate real api call
+  const fetchWizkids = () => {
+    setLoading(true);
+    setTimeout(async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/wizkids");
+        console.log(response.data);
+        setWizkids(response.data);
+      } catch(error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }, 1500);
+
+
   }
 
   const deleteWizkidById = async (id) => {
@@ -22,10 +37,28 @@ function Provider({children}) {
     setWizkids(updatedWizkids);
 };
 
+const editWizkidById = async (id, newName) => {
+  const response = await axios.put(`http://localhost:3001/wizkids/${id}`, {
+      name: newName
+  });
+  
+  const updatedWizkids = wizkids.map((wizkid) => {
+      if(wizkid.id === id) {
+          return { ...wizkid, ...response.data};
+      }
+      return wizkid;
+  });
+  
+  setWizkids(updatedWizkids);
+};
+
   const valueToShare = {
+    error: error,
+    loading: loading,
     wizkids: wizkids,
     fetchWizkids: fetchWizkids,
-    deleteWizkidById: deleteWizkidById
+    deleteWizkidById: deleteWizkidById,
+    editWizkidById: editWizkidById
   };
 
   return <WizkidsContext.Provider value={valueToShare}>
